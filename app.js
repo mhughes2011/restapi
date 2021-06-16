52a693ab33cd365ae0a446d822ecce5d27cda7b2
M Hughes <mhughes2011@gmail.com>
2021-06-16 11:13:33 -0700

    adjusted get routes to implement external data and async await

--- app.js
+++ app.js
@@ -1,19 +1,18 @@
 const express = require('express');
 const app = express();
 
-app.get('/greetings', (req, res) => {
-    res.json({greeting: 'Hello World?'})
-});
+const records = require('./records');
 
 //Send a GET request to /quotes READ a list of quotes
-app.get('/quotes', (req, res) => {
-    res.json(data);
+app.get('/quotes', async (req, res) => {
+    const quotes = await records.getQuotes();
+    res.json(quotes);
 });
 
 //Send a GET request to /quotes/:id READ (View) a quote
-app.get('/quotes/:id', (req, res) => {
-    let quote = data.quotes.find(quote => quote.id == req.params.id);
-    res.json(quote);
+app.get('/quotes/:id', async (req, res) => {
+    const singleQuote = await records.getQuote(req.params.id);
+    res.json(singleQuote);
 });
 
 //Send a POST request to /quotes CREATE a new quote
@@ -23,24 +22,3 @@ app.get('/quotes/:id', (req, res) => {
 
 
 app.listen(3000, () => console.log('Quote API listening on port 3000!'));
-
-
-const data = {
-    quotes: [
-      {
-        id: 8721,
-        quote: "We must accept finite disappointment, but we must never lose infinite hope.",
-        author: "Martin Luther King"
-      },
-      {
-        id: 5779,
-        quote: "Use what you’ve been through as fuel, believe in yourself and be unstoppable!",
-        author: "Yvonne Pierre"
-      },
-      {
-        id: 3406,
-        quote: "To succeed, you have to do something and be very bad at it for a while. You have to look bad before you can look really good.",
-        author: "Barbara DeAngelis"
-      }
-    ]
-  }
