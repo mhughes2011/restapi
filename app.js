18a491faf39da980a00b44db7c064ef082ffd2b1
M Hughes <mhughes2011@gmail.com>
2021-06-17 17:59:29 -0700

    added a post quote route and refactored all routes to use try catch

--- app.js
+++ app.js
@@ -3,19 +3,41 @@ const app = express();
 
 const records = require('./records');
 
+app.use(express.json());
+
 //Send a GET request to /quotes READ a list of quotes
 app.get('/quotes', async (req, res) => {
-    const quotes = await records.getQuotes();
-    res.json(quotes);
+    try{
+        const quotes = await records.getQuotes();
+        res.json(quotes);
+    }catch(err) {
+        res.json({message: err.message});
+    }
 });
 
 //Send a GET request to /quotes/:id READ (View) a quote
 app.get('/quotes/:id', async (req, res) => {
-    const singleQuote = await records.getQuote(req.params.id);
-    res.json(singleQuote);
+    try{
+        const singleQuote = await records.getQuote(req.params.id);
+        res.json(singleQuote);
+    }catch(err) {
+        res.json({message: err.message});
+    }
 });
 
 //Send a POST request to /quotes CREATE a new quote
+app.post('/quotes', async (req, res) => {
+    try{
+        const newQuote = await records.createQuote({
+            quote: req.body.quote,
+            author: req.body.author
+        });
+        res.json(newQuote);
+    }catch(err) {
+        res.json({message: err.message});
+    }
+});
+
 //Send a PUT request to /quotes/:id UPDATE (edit) a quote
 //Send a DELETE request to /quotes/:id DELETE a quote
 //Send a GET request to /quotes/quote/random READ (View) a random quote
