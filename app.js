e0e1d2fc68e788a5316bb900872242148cac035f
M Hughes <mhughes2011@gmail.com>
2021-06-18 15:18:09 -0700

    added a put route to update quotes

--- app.js
+++ app.js
@@ -47,6 +47,22 @@ app.post('/quotes', async (req, res) => {
 });
 
 //Send a PUT request to /quotes/:id UPDATE (edit) a quote
+app.put('/quotes/:id', async(req, res) => {
+    try {
+        const quote = await records.getQuote(req.params.id);
+        if(quote) {
+            quote.quote = req.body.quote;
+            quote.author = req.body.author;
+            await records.updateQuote(quote);
+            res.status(204).end();
+        } else {
+            res.status(404).json({message: 'Quote not found'});
+        }
+    } catch (err) {
+        res.status(500).json({message: err.message});
+    }
+})
+
 //Send a DELETE request to /quotes/:id DELETE a quote
 //Send a GET request to /quotes/quote/random READ (View) a random quote
 
