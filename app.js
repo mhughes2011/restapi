0ccf799dff0bcc614e15da5a4d12d8f9a7a3c51a
M Hughes <mhughes2011@gmail.com>
2021-06-21 16:34:32 -0700

    created a routes handler and adjusted the app accordingly

--- app.js
+++ app.js
@@ -1,106 +1,12 @@
 const express = require('express');
 const app = express();
-
-const records = require('./records');
-
-//Rather than adding a try catch block to every single call, this asyncHandler is created to allow the route to just call this function and handle all errors; example on post.quotes and put.quotes.:id routes.
-function asyncHandler(cb){
-    return async (req, res, next)=>{
-      try {
-        await cb(req,res, next);
-      } catch(err){
-        next(err);
-      }
-    };
-  }
+const routes = require('./routes');
 
 app.use(express.json());
-
-//Send a GET request to /quotes READ a list of quotes
-app.get('/quotes', async (req, res) => {
-    try{
-        const quotes = await records.getQuotes();
-        res.json(quotes);
-    }catch(err) {
-        res.json({message: err.message});
-    }
-})
-
-//Send a GET request to /quotes/:id READ (View) a quote
-app.get('/quotes/:id', async (req, res) => {
-    try{
-        const singleQuote = await records.getQuote(req.params.id);
-        if(singleQuote){
-            res.json(singleQuote);
-        } else {
-            res.status(404).json({message: "Quote not found."});
-        }
-    }catch(err) {
-        res.status(500).json({message: err.message});
-    }
-})
-
-//Send a POST request to /quotes CREATE a new quote
-// app.post('/quotes', async (req, res) => {
-//     try{
-//         if(req.body.author && req.body.quote) {
-//             const newQuote = await records.createQuote({
-//                 quote: req.body.quote,
-//                 author: req.body.author
-//             });
-//             res.status(201).json(newQuote);
-//         } else {
-//             res.status(400).json({message: "Quote and author required."});
-//         }
-//     }catch(err) {
-//         res.status(500).json({message: err.message});
-//     }
-// })
-
-app.post('/quotes', asyncHandler(async (req, res) => {
-    if(req.body.author && req.body.quote) {
-        const newQuote = await records.createQuote({
-            quote: req.body.quote,
-            author: req.body.author
-        });
-        res.status(201).json(newQuote);
-    } else {
-        res.status(400).json({message: "Quote and author required."});
-    }
-}));
-
-//Send a PUT request to /quotes/:id UPDATE (edit) a quote
-app.put('/quotes/:id', asyncHandler(async(req, res) => {
-    const quote = await records.getQuote(req.params.id);
-    if(quote) {
-        quote.quote = req.body.quote;
-        quote.author = req.body.author;
-        await records.updateQuote(quote);
-        res.status(204).end();
-    } else {
-        res.status(404).json({message: 'Quote not found'});
-    }
-}));
-
-//Send a DELETE request to /quotes/:id DELETE a quote
-app.delete('/quotes/:id', async(req, res) => {
-    try {
-        const quote = await records.getQuote(req.params.id);
-        if(quote) {
-            await records.deleteQuote(quote);
-            res.status(204).end();
-        } else {
-            res.status(404).json({message: 'Quote not found'});
-        }
-    } catch (err) {
-        res.status(500).json({message: err.message});
-    }
-})
-
-//Send a GET request to /quotes/quote/random READ (View) a random quote
+app.use('/api', routes);
 
 
-//This middleware is put at the end to handle any errors after all routes above have been handled.
+//This middleware is put at the end to handle any errors after all routes in the routes file have been handled.
 app.use((req, res, next) => {
     const err = new Error('Not Found');
     err.status = 404;
---
+++ routes.js
@@ -0,0 +1,100 @@
+const express = require('express');
+const router = express.Router();
+const records = require('./records');
+
+//Rather than adding a try catch block to every single call, this asyncHandler is created to allow the route to just call this function and handle all errors; example on post.quotes and put.quotes.:id routes.
+function asyncHandler(cb){
+    return async (req, res, next)=>{
+      try {
+        await cb(req,res, next);
+      } catch(err){
+        next(err);
+      }
+    };
+}
+
+//Send a GET request to /quotes READ a list of quotes
+router.get('/quotes', async (req, res) => {
+    try{
+        const quotes = await records.getQuotes();
+        res.json(quotes);
+    }catch(err) {
+        res.json({message: err.message});
+    }
+})
+
+//Send a GET request to /quotes/:id READ (View) a quote
+router.get('/quotes/:id', async (req, res) => {
+    try{
+        const singleQuote = await records.getQuote(req.params.id);
+        if(singleQuote){
+            res.json(singleQuote);
+        } else {
+            res.status(404).json({message: "Quote not found."});
+        }
+    }catch(err) {
+        res.status(500).json({message: err.message});
+    }
+})
+
+//Send a POST request to /quotes CREATE a new quote
+// router.post('/quotes', async (req, res) => {
+//     try{
+//         if(req.body.author && req.body.quote) {
+//             const newQuote = await records.createQuote({
+//                 quote: req.body.quote,
+//                 author: req.body.author
+//             });
+//             res.status(201).json(newQuote);
+//         } else {
+//             res.status(400).json({message: "Quote and author required."});
+//         }
+//     }catch(err) {
+//         res.status(500).json({message: err.message});
+//     }
+// })
+
+router.post('/quotes', asyncHandler(async (req, res) => {
+    if(req.body.author && req.body.quote) {
+        const newQuote = await records.createQuote({
+            quote: req.body.quote,
+            author: req.body.author
+        });
+        res.status(201).json(newQuote);
+    } else {
+        res.status(400).json({message: "Quote and author required."});
+    }
+}));
+
+//Send a PUT request to /quotes/:id UPDATE (edit) a quote
+router.put('/quotes/:id', asyncHandler(async(req, res) => {
+    const quote = await records.getQuote(req.params.id);
+    if(quote) {
+        quote.quote = req.body.quote;
+        quote.author = req.body.author;
+        await records.updateQuote(quote);
+        res.status(204).end();
+    } else {
+        res.status(404).json({message: 'Quote not found'});
+    }
+}));
+
+//Send a DELETE request to /quotes/:id DELETE a quote
+router.delete('/quotes/:id', async(req, res) => {
+    try {
+        const quote = await records.getQuote(req.params.id);
+        if(quote) {
+            await records.deleteQuote(quote);
+            res.status(204).end();
+        } else {
+            res.status(404).json({message: 'Quote not found'});
+        }
+    } catch (err) {
+        res.status(500).json({message: err.message});
+    }
+})
+
+//Send a GET request to /quotes/quote/random READ (View) a random quote
+
+
+module.exports = router;
